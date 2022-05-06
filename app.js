de3d968ba9abb31be3bf5c4c5bf7a4ecdef327cb
aoperat <54986046+aoperat@users.noreply.github.com>
2022-05-06 14:26:47 +0900

    add size
    Add width and height

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
 
+//사이즈입력
+canvas.width = 700;
+canvas.height = 700;
+
+
 ctx.strokeStyle ="#2c2c2c"
 ctx.lineWidth = 2.5;
 
