c4415efc13d725340b84e517ad676c679327159f
aoperat <54986046+aoperat@users.noreply.github.com>
2022-05-06 14:26:28 +0900

    ADD 2DContext

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle ="#2c2c2c"
+ctx.lineWidth = 2.5;
 
 //그림을 그리는 상태값
 let painting = false;
@@ -7,6 +11,11 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
+
 // offset 캔버스 부분과 관련있는 값
 // clientX,Y 윈도우 전체의 범위 내에서 마우스 위치값
 function onMouseMove(event){
@@ -14,21 +23,24 @@ function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
     
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else{
+        console.log(x,y)
+        ctx.lineTo(x,y)
+        ctx.stroke()
+    }
+
 }
 
 function onMouseDown(event){
     painting = true;
-    console.log(event);
-
-}
-
-function onMouseUp(event){
-    stopPainting();
 }
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedown",onMouseDown);
-    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mousedown",startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
